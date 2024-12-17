42a0356568a957e088ca0129ec2f0255fad0fa99
Kalash387 <kalashrami387@gmail.com>
2024-12-17 02:23:02 -0500

    testing deployment

--- backend/app.js
+++ backend/app.js
@@ -39,4 +39,5 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(MONGODB_URI)
 });
