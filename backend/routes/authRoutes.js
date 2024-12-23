eb80c30e7ccf02247af914de17474ded512d20cc
Kalash387 <kalashrami387@gmail.com>
2024-12-23 16:39:19 -0500

    debugging

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
 const { updateGame, getLastGame } = require('../controllers/gameController');
+const { auth } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Auth routes
@@ -9,7 +10,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 // Game routes
-router.post('/update-game', updateGame);
-router.get('/last-game', getLastGame);
+router.post('/update-game', auth, updateGame);
+router.get('/last-game', auth, getLastGame);
 
 module.exports = router;
